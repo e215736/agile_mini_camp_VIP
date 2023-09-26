cd0c29fa2096b87251959d589dbeaf82bf2765e4
Taiga Nishino <e215736@ie.u-ryukyu.ac.jp>
2023-09-26 16:13:53 +0900

    3rd commit:html imp

--- product/flask/testapp/views.py
+++ product/flask/testapp/views.py
@@ -1,5 +1,15 @@
 from testapp import app
+from flask import render_template  # 追加
 
 @app.route('/')
 def index():
-    return 'Hellow World!'
+    my_dict = {
+        'insert_something1': 'views.pyのinsert_something1部分です。',
+        'insert_something2': 'views.pyのinsert_something2部分です。',
+        'test_titles': ['title1', 'title2', 'title3']
+    }
+    return render_template('testapp/index.html', my_dict=my_dict)
+
+@app.route('/test')
+def other1():
+    return render_template('testapp/index2.html')
